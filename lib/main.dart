90b736b8b08d2d65797672af7a2d72a4af649a3d
nunchupachups <74181768+nunchupachups@users.noreply.github.com>
2021-12-07 18:29:38 +0700

    tictactoe

---
+++ lib/main.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+import 'package:tictactoe_tutorial/ui/theme/color.dart';
+import 'package:tictactoe_tutorial/utils/game_logic.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: GameScreen(),
+    );
+  }
+}
+
+class GameScreen extends StatefulWidget {
+  const GameScreen({Key? key}) : super(key: key);
+
+  @override
+  _GameScreenState createState() => _GameScreenState();
+}
+
+class _GameScreenState extends State<GameScreen> {
+  //adding the necessary variables
+  String lastValue = "X";
+  bool gameOver = false;
+  int turn = 0; // to check the draw
+  String result = "";
+  List<int> scoreboard = [
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0
+  ];
+
+  Game game = Game();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    game.board = Game.initGameBoard();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double boardWidth = MediaQuery.of(context).size.width;
+    return Scaffold(
+        appBar: AppBar(
+
+          backgroundColor: Colors.teal,
+          title: Align(
+              child:Text("Tic Tac Toe", style: TextStyle(color: Colors.white, fontSize: 30, ),),
+              alignment: Alignment.center)  ,
+        ),
+        backgroundColor: MainColor.primaryColor,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "It's ${lastValue} turn".toUpperCase(),
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+
+            Container(
+              width: boardWidth,
+              height: boardWidth,
+              child: GridView.count(
+                crossAxisCount: Game.boardlenth ~/3,
+                padding: EdgeInsets.all(16.0),
+                mainAxisSpacing: 8.0,
+                crossAxisSpacing: 8.0,
+                children: List.generate(Game.boardlenth, (index) {
+                  return InkWell(
+                    onTap: gameOver
+                        ? null
+                        : () {
+                            if (game.board![index] == "") {
+                              setState(() {
+                                game.board![index] = lastValue;
+                                turn++;
+                                gameOver = game.winnerCheck(
+                                    lastValue, index, scoreboard, 3);
+
+                                if (gameOver) {
+                                  result = "$lastValue is the Winner";
+                                } else if (!gameOver && turn == 9) {
+                                  result = "It's a Draw!";
+                                  gameOver = true;
+                                }
+                                if (lastValue == "X")
+                                  lastValue = "O";
+                                else
+                                  lastValue = "X";
+                              });
+                            }
+                          },
+                    child: Container(
+                      width: Game.blocSize,
+                      height: Game.blocSize,
+                      decoration: BoxDecoration(
+                        color: game.board![index] == ""
+                            ? MainColor.secondaryColor
+                            : (game.board![index] == "X"
+                            ? MainColor.accentColorX
+                            : MainColor.accentColorO),
+                        borderRadius: BorderRadius.circular(16.0),
+                      ),
+                      child: Center(
+                        child: Text(
+                          game.board![index],
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 64.0,
+                          ),
+                        ),
+                      ),
+                    ),
+                  );
+                }),
+              ),
+            ),
+            SizedBox(
+              height: 25.0,
+            ),
+            Text(
+              result,
+              style: TextStyle(color: Colors.white, fontSize: 30),
+            ),
+            ElevatedButton.icon(
+              onPressed: () {
+                setState(() {
+                  //erase the board
+                  game.board = Game.initGameBoard();
+                  lastValue = "X";
+                  gameOver = false;
+                  turn = 0;
+                  result = "";
+                  scoreboard = [0, 0, 0, 0, 0, 0, 0, 0];
+                });
+              },
+              icon: Icon(Icons.replay),
+              label: Text("Repeat"),
+            ),
+          ],
+        ));
+    //the first step is organise our project folder structure
+  }
+}
---
+++ lib/ui/theme/color.dart
@@ -0,0 +1,9 @@
+//now let's add the color that we will need to project
+import 'package:flutter/material.dart';
+
+class MainColor {
+  static Color primaryColor = Colors.black54;
+  static Color secondaryColor = Color(0xFFD0C4BE);
+  static Color accentColorX = Colors.red;
+  static Color accentColorO = Colors.black;
+}
